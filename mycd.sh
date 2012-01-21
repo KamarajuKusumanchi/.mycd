3da7a878f59e95d5257015710ae95c220017c61e
Jeremy Leipzig <leipzig@gmail.com>
2012-01-20 22:50:56 -0500

    cleanup

--- mycd.sh
+++ mycd.sh
@@ -2,14 +2,16 @@ function mycd()
 {
     tmpDir="$PWD"
     echo "#"`date +%s`" $USER -> $@"  >> "$HISTFILE"
-    #echo "$USER has exited $PWD for $@" >> "$HISTFILE"                                                   
+    
+    #a lifelong bash history
     history >> ~/Dropbox/work/forever
+    
     builtin cd "$@" # do actual cd                                                                        
-    #if this directory is writable then write to directory-based history file                             
-    #other write history in the usual home-based history file                                             
+    
+    #if this directory is writable then write to directory-based history file
+    #otherwise write history in the usual home-based history file                    
     touch "$PWD/.dir_bash_history" 2>/dev/null && export HISTFILE="$PWD/.dir_bash_history" || export HISTFILE="$HOME/.bash_history";
     echo "#"`date +%s`" $USER <- $OLDPWD" >> "$HISTFILE"
-    #echo "$USER <- $OLDPWD" >> "$HISTFILE"                                                               
 }
 alias cd="mycd"
 #initial shell opened                                                                                     
@@ -17,8 +19,8 @@ export HISTFILE="$PWD/.dir_bash_history"
 #timestamp all history entries                                                                            
 export HISTTIMEFORMAT="%h/%d - %H:%M:%S "
 export HISTCONTROL=ignoredups:erasedups
-export HISTSIZE=100000
-export HISTFILESIZE=100000
+export HISTSIZE=1000000
+export HISTFILESIZE=1000000
 shopt -s histappend ## append, no clearouts                                                               
 shopt -s histverify ## edit a recalled history line before executing                                      
 shopt -s histreedit ## reedit a history substitution line if it failed                                    
